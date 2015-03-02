d4241bfa3e272057fd879e9b649a57584b8c9e6e
unknown <vforventordici@gmail.com>
2015-03-02 15:10:34 +0100

    add menu + new Toolbar color

--- src/com/example/prova/MainActivity.java
+++ src/com/example/prova/MainActivity.java
@@ -2,7 +2,7 @@
 
 import java.util.List;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -21,7 +21,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.util.Log;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 	
 	TGDevice tgDevice; 
 	BluetoothAdapter btAdapter;
@@ -60,6 +60,25 @@ public class MainActivity extends Activity {
 		}
 	}
 	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 	
 	private final Handler handler = new Handler() { 
 		
